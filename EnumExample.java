f5aca68d5480e3a5b6654f123169b2dcbec2db33
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-08-03 10:41:52 +0530

    Day-2
    This is the program shows the use of enums

---
+++ EnumExample.java
@@ -0,0 +1,12 @@
+// This program demonstrates the use of enums in Java
+
+enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+public class EnumExample {
+    public static void main(String[] args) {
+        Day today = Day.WEDNESDAY;
+        System.out.println("Today is: " + today);
+    }
+}
