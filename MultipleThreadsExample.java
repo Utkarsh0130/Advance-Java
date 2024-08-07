809343d9504e2ddc4372a64f0b05d25d3b634d91
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-08-07 08:51:19 +0530

    Day-6
    This is program which shows how we can create multiple thread and to run them

---
+++ MultipleThreadsExample.java
@@ -0,0 +1,37 @@
+// This program demonstrates creating and running multiple threads in Java
+
+class MyThread1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread 1: " + i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread 2: " + i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class MultipleThreadsExample {
+    public static void main(String[] args) {
+        MyThread1 t1 = new MyThread1();
+        MyThread2 t2 = new MyThread2();
+
+        t1.start(); // Start thread 1
+        t2.start(); // Start thread 2
+    }
+}
